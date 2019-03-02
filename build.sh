dd7ac5aa73e196957053a3e5189d6088b3d415f1
uid09552 <max.rgbg@gmail.com>
2019-03-02 09:16:34 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker login -u $dockerId -p $pswd
+cd $0
+ID=$(docker build  -t $1:$2  .  | tail -1 | sed 's/.*Successfully built \(.*\)$/\1/')
+docker tag $(ID) $1:latest
+docker tag $(ID) $1:$2
+docker push $1:$2
+docker push $1:latest
