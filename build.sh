5f134d3d2689d46efb1e31adc0c2e4a066fbb9ab
uid09552 <max.rgbg@gmail.com>
2019-03-02 09:41:45 +0100

    Update build.sh
    id not needed

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 cd $1
 ID=$(docker build  -t $2:$3  .  | tail -1 | sed 's/.*Successfully built \(.*\)$/\1/')
-docker tag $ID $2:$3 $2:latest
+docker tag $2:$3 $2:latest
 docker push $2:$3
 docker push $2:latest
