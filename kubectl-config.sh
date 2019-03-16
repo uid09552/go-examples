87da1cf4fac204448c0ed2febc28bb8fe4e205fa
uid09552 <max.rgbg@gmail.com>
2019-03-16 11:09:14 +0100

    Update kubectl-config.sh
    fix for azure pipeline

--- kubectl-config.sh
+++ kubectl-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $server
-kubectl config set-cluster rancher --server=$server
+kubectl config set-cluster rancher --server=$VARIABLE_SERVER
 kubectl config set-context default --cluster rancher --user admin
-kubectl config set-credentials admin --token $token
+kubectl config set-credentials admin --token $VARIABLE_TOKEN
 kubectl config use-context default
