dfa3ecbe351719e9a4ac71f752ddefa3ae34d242
uid09552 <max.rgbg@gmail.com>
2019-03-16 12:19:41 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ echo "common deploy functions"
 kubectl version
 kubectl apply -f $1
 kubectl describe deployment $2
-kubectl label deployment $2 version=$RELEASE_ENVIRONMENTID
-kubectl label deployment $2 deploymenttime=$RELEASE_DEPLOYMENT_STARTTIME
+kubectl label --overwrite deployment $2 version=$RELEASE_ENVIRONMENTID
+kubectl label --overwrite deployment $2 deploymenttime=$RELEASE_DEPLOYMENT_STARTTIME
 kubectl describe deployment $2
