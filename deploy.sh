b6913bf3600ad21687664f0b5cc3305ddd479c73
uid09552 <max.rgbg@gmail.com>
2019-03-16 12:16:58 +0100

    Update deploy.sh
    bug

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo "common deploy functions"
 kubectl version
 kubectl apply -f $1
-docker describe deployment $2
+kubectl describe deployment $2
 kubectl label deployment $2 version=$RELEASE_ENVIRONMENTID
 kubectl label deployment $2 deploymenttime=$RELEASE_DEPLOYMENT_STARTTIME
 kubectl describe deployment $2
