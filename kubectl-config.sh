3342184fe9591e39499745d1a06e1a96799a1922
uid09552 <max.rgbg@gmail.com>
2019-03-16 10:41:25 +0100

    Create kubectl-config.sh

---
+++ kubectl-config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+kubectl config set-cluster rancher --server=$server
+kubectl config set-context default --cluster rancher --user admin
+kubectl config set-credentials admin --token $token
+kubectl config use-context default
