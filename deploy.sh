5c27528a673619d5d7145fef821b1f2b15522e71
uid09552 <max.rgbg@gmail.com>
2019-03-16 14:05:42 +0100

    Update deploy.sh
    adding buildnumber

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ kubectl version
 kubectl apply -f $1
 kubectl describe deployment $2
 kubectl label --overwrite deployment $2 version=$RELEASE_ENVIRONMENTID
+kubectl label --overwrite deployment $2 build="$BUILD_BUILDNUMBER"
 echo "Deploymentdate"$RELEASE_DEPLOYMENT_STARTTIME
 kubectl annotate --overwrite deployment $2 deploymenttime="$RELEASE_DEPLOYMENT_STARTTIME"
 kubectl describe deployment $2
