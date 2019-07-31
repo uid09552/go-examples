344596dbba40f3ac1b3da16eb56c92ef72fffdfc
uid09552 <max.rgbg@gmail.com>
2019-07-31 20:12:00 +0200

    gateway, + gin example

---
+++ docker/gin/src/gin/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+type Note struct {
+	Creator string `json:"creator"`
+	Text    string `json:"text" binding:"required"`
+}
+
+var notes = []Note{Note{Creator: "Auto", Text: "Exampole"}, Note{Creator: "Auto", Text: "Example2"}}
+
+func main() {
+	fmt.Println("Starting")
+	router := gin.Default()
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+		api.GET("/notes", NotesHandler)
+		api.POST("/notes", NotesHandler)
+	}
+	router.Run(":3000")
+}
+
+func NotesHandler(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	if c.Request.Method == http.MethodGet {
+		c.JSON(http.StatusOK, notes)
+	}
+}
