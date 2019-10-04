ade4991729572d16652a24f45223d7cc3dbea214
uid09552 <max.rgbg@gmail.com>
2019-10-04 11:20:58 +0200

    Update kubectl-config.sh

--- kubectl-config.sh
+++ kubectl-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo $server
 kubectl config set-cluster rancher --server=$VARIABLE_SERVER
-kubectl config set-context default --cluster rancher --user admin
+kubectl config set-context default --cluster rancher --user admin-user
 kubectl config set-credentials admin --token $VARIABLE_TOKEN
 kubectl config use-context default
 kubectl get nodes
