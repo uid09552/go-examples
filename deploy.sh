fb5868b1b1e8b5f3ac397b5a6617a44c7e2ba392
uid09552 <max.rgbg@gmail.com>
2020-03-01 19:53:33 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,3 @@ kubectl label --overwrite deployment $2 build="$BUILD_BUILDNUMBER"
 echo "Deploymentdate"$RELEASE_DEPLOYMENT_STARTTIME
 kubectl annotate --overwrite deployment $2 deploymenttime="$RELEASE_DEPLOYMENT_STARTTIME"
 kubectl describe deployment $2
-
-cd kafka/no-persist
-kubectl apply -f kafka-deploy.yml
-kubectl apply -f kafka-service.yml
-kubectl apply -f zoo-deploy.yml
-cd ../..
